35763d4390b10d390a5fb591e51942dbe5c53dba
vorn-tovtrip <sovanpanhavorn.siv@tovtrip.com>
2025-06-17 23:19:24 +0700

    config meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,20 @@ import TanStackProvider from "./tanstack";
 import ToastProvider from "./toastprovider";
 
 export const metadata: Metadata = {
-  title: "Korean Product",
+  title: "GenzEcommerce Product",
   description: "this is a description",
+  openGraph: {
+    title: "Genz Ecommerce Product",
+    description: "this is a description",
+    images: [
+      {
+        url: "https://d23k61tiwy13nu.cloudfront.net/66bb9e85eba9d69dd1c9d76ec29fc7d9ef3d6828_1024x504_1_a4ecfea89c.png",
+        width: 1200,
+        height: 630,
+        alt: "Genz Ecommerce Product Image",
+      },
+    ],
+  },
 };
 const MontserratFont = Montserrat({
   subsets: ["latin"],
