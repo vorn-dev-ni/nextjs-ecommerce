9f352df8df1b44852bc30826f953ef85acc914d9
vorn-tovtrip <sovanpanhavorn.siv@tovtrip.com>
2025-06-18 11:10:36 +0700

    disable check

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
         hostname: "**",
         port: "",
       },
+
       {
         protocol: "https",
         hostname: "d23k61tiwy13nu.cloudfront.net",
